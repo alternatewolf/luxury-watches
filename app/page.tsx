79c0182c2662d9251bad500f4ffa291e358e63b1
rayjay8 <rayjay9@proton.me>
2025-05-21 01:13:45 +0530

    images

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="relative bg-[#F8F5EE]">
           <div className="absolute inset-0 bg-black/5" />
           <img
-            src="https://www.breda.com/cdn/shop/files/breda-jane-1741c-fall-3-2023-gold-metal-bracelet-watch-studio-01_e6ee2eb7-61b2-4c1b-b1ad-40501395366b_1440x.jpg?v=1696518796"
+            src="https://media.fastdl.app/get?__sig=2CDNO8OEVLK2uv6sElOlBA&__expires=1747770325&uri=https%3A%2F%2Finstagram.fphl1-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F499552837_18508368010027120_8679493673769328971_n.jpg%3Fstp%3Ddst-jpg_e15_fr_p1080x1080_tt6%26_nc_ht%3Dinstagram.fphl1-1.fna.fbcdn.net%26_nc_cat%3D104%26_nc_oc%3DQ6cZ2QFGsBWf20wpM6g1dLA7gbTOqioCgo1ekvIwYXorBl0BuQSPqCh87M9GKfDQp7Vd3VuSLoYjEK60rwgBDdrZOCQW%26_nc_ohc%3DK5n13pmEf9oQ7kNvwFVE5vB%26_nc_gid%3DBUzzBUZAjsmP_vr1GsTCBQ%26edm%3DANTKIIoBAAAA%26ccb%3D7-5%26oh%3D00_AfJFktd-yFVcwEzJkrXR60OX5F168IBGnq9qbsCspVCOwg%26oe%3D68329A6E%26_nc_sid%3Dd885a2&filename=499552837_18508368010027120_8679493673769328971_n.jpg"
             alt="Luxury Watch"
             className="w-full h-full object-cover"
           />
@@ -19,7 +19,7 @@ export default function Home() {
         <div className="relative bg-[#F8F5EE]">
           <div className="absolute inset-0 bg-black/5" />
           <img
-            src="https://www.breda.com/cdn/shop/files/breda-pulse-tandem-1747b-fall-3-2023-silver-metal-bracelet-watch-lifestyle-06_1440x.jpg?v=1707945649"
+            src="https://media.fastdl.app/get?__sig=c5Lu-sZv6UOynDJZIlUXXg&__expires=1747769932&uri=https%3A%2F%2Finstagram.fphl1-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2F491438518_18522501988049800_4642885160736309726_n.jpg%3Fstp%3Ddst-jpg_e15_tt6%26_nc_ht%3Dinstagram.fphl1-1.fna.fbcdn.net%26_nc_cat%3D104%26_nc_oc%3DQ6cZ2QG_cmmgS0hT4FIGSLWgRC9f4DJw4l3GQ7GBZmpJl3TqNpH7r9PLidxencFmZ2DhEoM3Eghv2VynpiKuLGGkpyh3%26_nc_ohc%3DscT2v_cjTREQ7kNvwEJF0in%26_nc_gid%3DP80FMnFVYI3WiuJcQeO_zQ%26edm%3DANTKIIoBAAAA%26ccb%3D7-5%26oh%3D00_AfJgF57jJFUEAG4IEYLr4p_LQiAEQcwn09kdr_Sx9SXgMw%26oe%3D6832A8F6%26_nc_sid%3Dd885a2&filename=491438518_18522501988049800_4642885160736309726_n.jpg"
             alt="Luxury Watch"
             className="w-full h-full object-cover"
           />
@@ -30,7 +30,7 @@ export default function Home() {
       <section className="h-[100vh] mt-0.5 relative bg-[#F8F5EE]">
         <div className="absolute inset-0 bg-black/5" />
         <img
-          src="https://www.breda.com/cdn/shop/files/breda-jane-1741c-fall-3-2023-gold-metal-bracelet-watch-studio-01_e6ee2eb7-61b2-4c1b-b1ad-40501395366b_1440x.jpg?v=1696518796"
+          src="https://images.unsplash.com/photo-1564595076323-71cc27edacf8?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Luxury Watch"
           className="w-full h-full object-cover"
         />
